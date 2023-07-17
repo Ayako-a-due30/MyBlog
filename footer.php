08ed64c1d9e02584c4eefbbbd71c8439470cce6c
Ayako-a-due30 <sonnige.seite.str@gmail.com>
2023-07-17 16:53:01 +0900

    MyBlog

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="<?php echo get_template_directory_uri();?>/jquery/jquery.min.js"></script>
+  <script src="<?php echo get_template_directory_uri();?>/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="<?php echo get_template_directory_uri();?>/clean-blog.min.js"></script>
+
+  <?php wp_footer(); ?>
